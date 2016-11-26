46a504c8ff9f71d5eb878265d980c7655156842a
GigaFlower <a_gigantic_flower@yeah.net>
2016-11-26 15:49:08 +0800

    weixin process

--- hackthon/main.py
+++ hackthon/main.py
@@ -3,7 +3,9 @@
 
 from flask import Flask, request, render_template
 
-from .utility import emotion_api
+from .utility import emotion_api, wx_get_media, wx_put_media, photo_to_video
+from .wxreceive import parse_xml, Msg
+from .wxreply import VideoMsg
 
 app = Flask(__name__)
 
@@ -13,12 +15,39 @@ def hello_world():
     return "如果你看到了本页面\n,那说明你有网"
 
 
-@app.route('/wx', methods=['GET'])
-def wx():
+@app.route('/wx', methods=['GET', 'POST'])
+def wx_verify():
     print(request.data)
     print(request.args)
     print(request.method)
-    return ''
+    return '这里是该死的微信端口'
+
+
+def wx_handle():
+    try:
+        data = request.data
+        print("Handle Post webdata is ", data)
+        msg = parse_xml(data)
+        if isinstance(msg, Msg):
+            toUser = msg.FromUserName
+            fromUser = msg.ToUserName
+            # if msg.MsgType == 'text':
+            #     content = "test"
+            #     return rTextMsg(toUser, fromUser, content).send()
+            assert msg.MsgType == 'image'
+            mediaId = msg.MediaId
+            media = wx_get_media('', mediaId)
+            video = photo_to_video(media)
+            video_id = wx_put_media('', video, media_type='voice')['media_id']
+            return VideoMsg(toUser, fromUser, mediaId).send()
+            # else:
+            #     print("Unknown msg type")
+            #     return "success"
+        else:
+            print("暂且不处理")
+            return "success"
+    except Exception as e:
+        return e
 
 
 @app.route('/test', methods=['GET'])
--- hackthon/utility.py
+++ hackthon/utility.py
@@ -11,8 +11,46 @@ def emotion_api(photo):
                              headers={'Content-Type': 'application/octet-stream',
                                       'Ocp-Apim-Subscription-Key': '13b113fed5884aa998de124391a004f3'}
                              )
-        resp = resp.text
     except Exception as e:
         return ''
     else:
-        return resp
+        return resp.content
+
+
+def wx_get_media(access_token, media_id):
+    url = "https://api.weixin.qq.com/cgi-bin/media/get?access_token={}&media_id={}".format(access_token, media_id)
+    try:
+        resp = requests.get(url)
+    except Exception as e:
+        print("wx get media failed")
+        print(e)
+        return b''
+    else:
+        print("wx get media ret:")
+        print(resp.content)
+        return resp.content
+
+
+def wx_put_media(access_token, data, media_type='voice'):
+    url = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token={}&type={}".format(access_token, media_type)
+
+    try:
+        resp = requests.post(url, data)
+    except Exception as e:
+        print("wx get media failed")
+        print(e)
+        return b''
+    else:
+        print("wx get media ret:")
+        print(resp.content)
+        return resp.content
+
+
+def photo_to_video(photo):
+    emotion = emotion_api(photo)
+    video = emotion_to_video(emotion)
+    return video
+
+
+def emotion_to_video(emotion):
+    pass
